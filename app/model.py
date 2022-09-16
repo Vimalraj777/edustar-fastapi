bacbfeddd880c797de98839f1f527db47f2a7dff
vimal <vimal@dbcyelagiri.edu.in>
2022-09-16 18:58:48 +0530

    added docker

--- app/model.py
+++ app/model.py
@@ -322,10 +322,4 @@ class Vote(Base):
     __tablename__="vote"
     posts_id=Column(Integer,ForeignKey("posts.id",ondelete='CASCADE'),primary_key=True)
     users_id=Column(Integer,ForeignKey("users.id",ondelete='CASCADE'),primary_key=True)
-
-
-
-
-
-
-
+    
